f7687f794aaa33a243d6c27a60878ddd0e999e68
zenglj <zenglj@live.com>
2024-11-25 08:26:10 +0800

    主流程代码优化

--- frontend/Graph.cpp
+++ frontend/Graph.cpp
@@ -57,7 +57,7 @@ string getNodeName(ast_node * astnode)
             nodeName = "compile-unit";
             break;
         case ast_operator_type::AST_OP_FUNC_FORMAL_PARAMS:
-            nodeName = "formal_params";
+            nodeName = "formal-params";
             break;
             // TODO 这里追加其它类型的结点，返回对应结点的字符串
 
